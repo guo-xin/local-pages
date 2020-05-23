2e0dbbd8e248240774613a5bf199813dc7bc6ac2
guoxin <guoxin16@guazi.com>
2020-05-23 21:21:14 +0800

    for-each

--- html/woyouyisheng/js/public.js
+++ html/woyouyisheng/js/public.js
@@ -20,14 +20,8 @@ $(function() {
 function isPC() {
 	var userAgentInfo = navigator.userAgent;
 	var Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
-	var flag = true;
-	for(var v = 0; v < Agents.length; v++) {
-		if(userAgentInfo.indexOf(Agents[v]) > 0) {
-			flag = false;
-			break;
-		}
-	}
-	return flag;
+
+	return !Agents.includes(userAgentInfo);
 }
 
 //错误信息提示
---
+++ mock/listData.js
@@ -0,0 +1,23 @@
+let listData = [
+    {
+        name: 'guoxin',
+        value: 11111
+    },
+    {
+        name: 'hanmengqi',
+        value: 22222
+    },
+    {
+        name: 'guoyuhao',
+        value: 3333
+    },
+    {
+        name: 'guoyuhao',
+        value: 44444
+    },
+    {
+        name: 'guoyuhao',
+        value: 55555
+    }
+];
+
