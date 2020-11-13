1661957bf2dddb9afcfd6d720d93becec186cf56
guoxin16 <guoxin16@guazi.com>
2020-11-13 18:35:47 +0800

    feature: 攒了好久的,先提交下

--- mock/listData.js
+++ mock/listData.js
@@ -1,23 +1,22 @@
 let listData = [
     {
-        name: 'guoxin',
-        value: 11111
+        name: "guoxin",
+        value: 11111,
     },
     {
-        name: 'hanmengqi',
-        value: 22222
+        name: "hanmengqi",
+        value: 22222,
     },
     {
-        name: 'guoyuhao',
-        value: 3333
+        name: "guoyuhao",
+        value: 3333,
     },
     {
-        name: 'guoyuhao',
-        value: 44444
+        name: "guoyuhao",
+        value: 44444,
     },
     {
-        name: 'guoyuhao',
-        value: 55555
-    }
+        name: "guoyuhao",
+        value: 55555,
+    },
 ];
-
